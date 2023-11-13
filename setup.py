c6b085e6af36a5825341ed21207c15561795759d
jjkaass <hj37078119@gmail.com>
2023-11-13 22:27:21 +0100

    after Add requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,16 @@
 from setuptools import setup, find_packages
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(
     name='math_quiz',
     version='0.1.0',
     packages=find_packages(),
-    install_requires=[
-        # List your dependencies here
-        # 'numpy',
-        # 'requests',
-    ],
+    install_requires=requirements,
+    # install_requires=[
+    #     # List your dependencies here
+    #     # 'numpy',
+    #     # 'requests',
+    # ],
 )
