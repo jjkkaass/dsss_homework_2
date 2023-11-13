9750492a74e9f8649e66a1e5dd16b8de9527225f
jjkaass <hj37078119@gmail.com>
2023-11-13 17:28:19 +0100

    remove！

--- math_quiz/math_quiz.py
+++ math_quiz/math_quiz.py
@@ -1,4 +1,5 @@
 import random
+# Jin Huang / an46ykim /23079099
 
 
 def generate_random_integer(minimum, maximum):
@@ -14,13 +15,6 @@ def generate_random_operator():
     """
     return random.choice(['+', '-', '*'])
 
-0
-# def F3(n1, n2, n):
-#     p = f"{n1} {o} {n2}"
-#     if o == '+': a = n1 - n2
-#     elif o == '-': a = n1 + n2
-#     else: a = n1 * n2
-#     return p, a
 
 def perform_operation(num1, num2, operator):  # Change variable names
     # p = f"{n1} {n3} {n2}"
@@ -57,21 +51,6 @@ def math_quiz():
     print("Welcome to the Math Quiz Game!")
     print("You will be presented with math problems, and you need to provide the correct answers.")
 
-    # for _ in range(total_questions):
-    #     n1 = F1(1, 10); n2 = F1(1, 5.5); n3 = F2()
-    #
-    #     PROBLEM, ANSWER = F3(n1, n2, n3)
-    #     print(f"\nQuestion: {PROBLEM}")
-    #     useranswer = input("Your answer: ")
-    #     useranswer = int(useranswer)
-    #
-    #     if useranswer == ANSWER:
-    #         print("Correct! You earned a point.")
-    #         score += -(-1)
-    #     else:
-    #         print(f"Wrong answer. The correct answer is {ANSWER}.")
-    #
-    # print(f"\nGame over! Your score is: {s}/{t_q}")
     try:
         for _ in range(total_questions):
             num1 = generate_random_integer(1, 10)
